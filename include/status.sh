8eabcafb8eb6fc6fe00d275f7e7f16c60e60e6ab
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-02-24 04:13:24 +0100

    Fix codegen bug.

--- include/status.sh
+++ include/status.sh
@@ -1,21 +1,26 @@
-issues_get_config
-issues_codegen_statuses=""
-issues_asterisk='*'
-for s in ${issues_statuses[@]}; do
-  issues_codegen_statuses+="
-    issues_${s}() {
-      issues_status_do \"${s}\" \$*
-    };
-    "
-done
+if [ -f ".issues/config" ]; then
+  issues_get_config
+  issues_codegen_statuses=""
+  issues_asterisk='*'
+  for s in ${issues_statuses[@]}; do
+    issues_codegen_statuses+="
+      issues_${s}() {
+        issues_status_do \"${s}\" \$*
+      };
+      "
+  done
 
-eval "$issues_codegen_statuses"
+  eval "$issues_codegen_statuses"
 
-issues_status_do() {
-  new_status=$1 && shift
-  [ -z "$1" ] && err "Id chain (like 'wasd/x0c3') is required for setting status to an issue" || path="$1"
-  [ ! -d "$issues_root/$path" ] && err "$path is not a valid id chain"
-  issues_fetch
-  echo -n "$new_status" > "$issues_root/$path/status"
-  issues_git_push "Set status for #$(basename $path) to $new_status"
-}
+  issues_status_do() {
+    new_status=$1 && shift
+    [ -z "$1" ] && err "Id chain (like 'wasd/x0c3') is required for setting status to an issue" || path="$1"
+    [ ! -d "$issues_root/$path" ] && err "$path is not a valid id chain"
+    issues_fetch
+    echo -n "$new_status" > "$issues_root/$path/status"
+    issues_git_push "Set status for #$(basename $path) to $new_status"
+  }
+else
+  echo "[warn] Status handling code can't be generated, no config supplied."
+  export issues_statuses=()
+fi
