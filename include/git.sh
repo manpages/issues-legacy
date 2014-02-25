7a04cf8403dc567c230d5112c4ef494662315a18
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-02-25 23:25:04 +0100

    Remove unneeded code; fix issues_git_push function

--- include/git.sh
+++ include/git.sh
@@ -7,5 +7,8 @@ issues_git_headhash() {
 }
 
 issues_git_push() {
-  ( cd $issues_root && git add . && git commit -am "$1" && git push )
+  # It works because issues repo is strictly linear and append-only.
+  # We will never get merge conflicts. A nasty case would be when two people are making
+  # an issue with the same ID chain but this should be resolved manually anyway
+  ( cd $issues_root && git pull && git add . && git commit -am "$1" && git push )
 }
