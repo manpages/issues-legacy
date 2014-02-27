d1876216d85ce62fcf86e39e9feaff2b2bda3048
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-02-27 01:11:31 +0100

    More complete tag functionality

--- include/tag.sh
+++ include/tag.sh
@@ -1,5 +1,6 @@
 issues_tag() {
-  [ -z "$1" ] && err "Provide id chain is needed to tag an issue" || issue="$issues_root/$1"
+  arg1="$1"
+  [ -z "$1" ] && err "An id chain is needed to tag an issue" || issue="$issues_root/$1"
   shift
   [ -z "$1" ] && err "What do you want to tag the issue with?"    || tag="$*"
   [ -d "$issue" ]      || err "Not a valid id chain"
@@ -10,9 +11,22 @@ issues_tag() {
   debug "Unique tags now: $(printf "%q\n" "${tags[@]}" | sort -u)"
   eval tags=($(printf "%q\n" "${tags[@]}" | sort -u))
   printf "%q," ${tags[@]} > "$issue/tags"
-  issues_rmtag "untagged"
+  issues_rmtag "$arg1" "untagged" # dirty dirty
 }
 
 issues_rmtag() {
-  debug "issues_rmtag function is not yet implemented (attempted call: issues_rmtag $*)"
+  debug "issues_rmtag $*"
+  [ -z "$1" ] && err "An id chain is needed to tag an issue" || issue="$issues_root/$1"
+  shift
+  [ -z "$1" ] && err "Which tag do you want to remove?"      || tag="$*"
+  debug "Issue: $issue"
+  [ -d "$issue" ] || err "Not a valid id chain"
+  [ -f "$issue/tags" ] || err "You can't tag comments nowadays, only issues. This limitation might be lifted someday."
+  debug "$issue/tags: $(cat $issue/tags)"
+  IFS=, read -ra tags <<< "$(cat $issue/tags)"
+  tags1=(  )
+  for i in ${tags[@]}; do
+    [ i == $tag ] || tags1=( ${tags1[@]} "$i" )
+  done
+  printf "%q," ${tags1[@]} > "$issue/tags"
 }
