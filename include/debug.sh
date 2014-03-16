9c2e59b1cb471ff326a776b8c2c57c1f0e4db8ba
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-03-16 22:09:01 +0100

    Fix debug bug.

--- include/debug.sh
+++ include/debug.sh
@@ -1,14 +1,9 @@
 issues_set_debug_level() {
-  echo "SETTING ISSUES_DEBUG_LEVEL TO 1"
   export ISSUES_DEBUG_LEVEL=1
   [ $1 == "none" ] && export ISSUES_DEBUG_LEVEL=0 && return
-  echo "SETTING ISSUES_DEBUG_LEVEL TO 0"
   [ $1 == "errors" ] && export ISSUES_DEBUG_LEVEL=1 && return 
-  echo "SETTING ISSUES_DEBUG_LEVEL TO 2"
   [ $1 == "messages" ] && export ISSUES_DEBUG_LEVEL=2 && return
-  echo "SETTING ISSUES_DEBUG_LEVEL TO 3"
   [ $1 == "info" ] && export ISSUES_DEBUG_LEVEL=3 && return
-  echo "SETTING ISSUES_DEBUG_LEVEL TO 4"
   [ $1 == "debug" ] && export ISSUES_DEBUG_LEVEL=4 && return
 }
 
